8ed2f10abf10b260dd01b41e0dadd72774653bb9
Franco <96508706+FrancoRamirezz@users.noreply.github.com>
2024-05-27 20:25:48 -0700

    Update script.py

---
+++ script.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "put the website vercel here once done"
+# goal is to scrape data from the website to see how is your website doing 
+
+
+page = requests.get(url)
